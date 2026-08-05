c6b11739d0171cba273c2264182a1bbfda6211be
Mansoor Ali Shaik <73877240+mansoorali1@users.noreply.github.com>
2026-08-05 13:31:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,5 @@ you don't want or need once this is actually deployed somewhere).
 from app.ui import demo
 
 if __name__ == "__main__":
-    demo.launch(
-        server_name="0.0.0.0",
-        server_port=7860,
-        show_error=True,
-    )
-
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=port, show_error=True)
